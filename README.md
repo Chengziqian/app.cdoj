7004edab90141e925b90889c184d2f6543f9bd44
GuessEver <guessever@gmail.com>
2017-08-21 15:46:03 +0800

    init vue

---
+++ README.md
@@ -0,0 +1,25 @@
+# app.cdoj
+
+> web app for cdoj
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# edit configure
+cp .config.js.example .config.js
+# now edit .config.js
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/main.js
@@ -0,0 +1,32 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+import 'normalize.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = '/api/'
+Vue.$http = Vue.prototype.$http = axios
+
+import lodash from 'lodash'
+window._ = Window.prototype._ = lodash
+Vue._ = Vue.prototype._ = lodash
+
+import router from './router'
+router.beforeEach((to, from, next) => {
+  next()
+})
+router.afterEach(route => {
+  window.scrollTo(0, 0)
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App></App>',
+  components: { App }
+})
